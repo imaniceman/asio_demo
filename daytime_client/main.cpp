237dcaacb9702aee986982c22a7be2fc7db1fffe
liyixin123 <592945148@qq.com>
2024-10-08 14:59:32 +0800

    同步时间获取客户端和服务完成

---
+++ daytime_client/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: " << argv[0] << " <host>" << '\n';
+            return 1;
+        }
+        boost::asio::io_context ioContext;
+        tcp::resolver resolver(ioContext);
+        // Resolve the host name into an IP address that can be used to connect to the server
+        tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
+
+        // Create and connect a socket
+        tcp::socket socket(ioContext);
+        boost::asio::connect(socket, endpoints);
+
+        for (;;)
+        {
+            std::array<char, 128> buf;
+            boost::system::error_code error;
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            if (error == boost::asio::error::eof)
+            {
+                break;
+            }
+            else if (error)
+            {
+                throw boost::system::system_error(error);
+            }
+            std::cout.write(buf.data(), len);  // NOLINT(bugprone-narrowing-conversions, cppcoreguidelines-narrowing-conversions)
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    return 0;
+}
---
+++ daytime_server/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <array>
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <boost/asio.hpp>
+using boost::asio::ip::tcp;
+
+std::string makeDaytimeString()
+{
+    time_t now = time(nullptr);
+    std::array<char, 128> buf;
+    if (auto err = ctime_s(buf.data(), buf.size(), &now); err != 0)
+    {
+        return "Error";
+    }
+    return buf.data();
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        boost::asio::io_context ioContext;
+        tcp::acceptor acceptor(ioContext, tcp::endpoint(tcp::v4(), 13));
+        for(;;)
+        {
+            tcp::socket socket(ioContext);
+            acceptor.accept(socket);
+            std::string message = makeDaytimeString();
+            boost::system::error_code ignoredError;
+            boost::asio::write(socket, boost::asio::buffer(message), ignoredError);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return 0;
+}
